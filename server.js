2131100a91e85d62294c029dd5b7f599e6963f7e
Diego Matias <dm.diego.bh@gmail.com>
2018-09-26 20:36:46 -0300

    add dot env

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -12,24 +14,26 @@ const app = express();
 const port=3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
+if(process.env.HTTP=='false'){
+    app.get('*', (req, res, next) => {
+        if (req.headers['x-forwarded-proto'] != 'https') {
+            
+    // checa se o header é HTTP ou HTTPS
+    
+            res.redirect("https://" + req.headers.host + req.url);
+            
+    // faz o redirect para HTTPS
+    
+        } else {
+            next();
+            
+    // segue com a sequência das rotas
+    
+        }
+    });
+}
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-app.get('*', (req, res, next) => {
-    if (req.headers['x-forwarded-proto'] != 'https') {
-        
-// checa se o header é HTTP ou HTTPS
-
-        res.redirect("https://" + req.headers.host + req.url);
-        
-// faz o redirect para HTTPS
-
-    } else {
-        next();
-        
-// segue com a sequência das rotas
-
-    }
-});
 
 require('./app/routes/index')(app,connection);
 
