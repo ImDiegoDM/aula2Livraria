1cb1f54d94c1a45ede563e700bdb04e9c993b2c4
Diego Matias <dm.diego.bh@gmail.com>
2018-09-26 20:19:12 -0300

    small fixes

--- server.js
+++ server.js
@@ -14,6 +14,23 @@ const port=3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+app.get('*', (req, res, next) => {
+    if (req.headers['x-forwarded-proto'] != 'https') {
+        
+// checa se o header é HTTP ou HTTPS
+
+        res.redirect("https://" + req.headers.host + req.url);
+        
+// faz o redirect para HTTPS
+
+    } else {
+        next();
+        
+// segue com a sequência das rotas
+
+    }
+});
+
 require('./app/routes/index')(app,connection);
 
 app.listen(port,()=>{
